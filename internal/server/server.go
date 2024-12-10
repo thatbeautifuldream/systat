40873490d3a0f0fdd65cf5830b9c944f4021c60c
Milind Mishra <milind.mishra4@gmail.com>
2024-12-11 02:54:29 +0530

    Initial commit

---
+++ internal/server/server.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type FiberServer struct {
+	*fiber.App
+}
+
+func New() *FiberServer {
+	server := &FiberServer{
+		App: fiber.New(fiber.Config{
+			ServerHeader: "systat",
+			AppName:      "systat",
+		}),
+	}
+
+	return server
+}
